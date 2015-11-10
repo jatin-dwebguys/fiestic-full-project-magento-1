cbad2b715c39eb7de985f4cf786835ef2750e9e5
manish <manish>
2015-11-10 15:41:57 +0530

    done product page

--- app/code/local/Fiestic/Ingram/controllers/IndexController.php
+++ app/code/local/Fiestic/Ingram/controllers/IndexController.php
@@ -3,14 +3,16 @@
 class Fiestic_Ingram_IndexController extends Mage_Core_Controller_Front_Action {
 
     public function indexAction() {
-
-	Mage::getModel('ingram/shop')->getProductData('110188696X');
+	$isbnNo = 'BN=' . $this->getRequest()->getParam('BN');
+	Mage::getModel('ingram/shop')->getProductData($isbnNo);
+//product = zMage::registry('product');
+    	//echo '<pre>';print_r($_product);die; 
           /*echo '<pre>';
           print_r($ingramSearch);
           die; //*/
         $this->loadLayout();
         $this->renderLayout();
     }
-
+ 
 }
 
