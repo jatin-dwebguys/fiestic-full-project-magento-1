9c1c796d05d56183f56af328cfd96c7c16a6dedf
manish <manish>
2015-11-13 14:32:59 +0530

    working on category view page

--- app/code/core/Mage/Catalog/controllers/CategoryController.php
+++ app/code/core/Mage/Catalog/controllers/CategoryController.php
@@ -40,6 +40,34 @@ class Mage_Catalog_CategoryController extends Mage_Core_Controller_Front_Action
      */
     protected function _initCatagory()
     {
+	
+// code added for server categories 
+ $name = Mage::getModel('ingram/shop')->getProductData($isbnNo, $type);
+
+ $client = new SoapClient("http://idswebtest.ingramcontent.com/cws/companion.asmx?wsdl");
+
+        $userInfo = array('UserName' => 'F1eSt1c_1278', 'Password' => 'p@ssW0rd');
+
+        $headers = new SoapHeader('http://ingrambook.com/CompDataAccess/companion', 'UserInfo', $userInfo);
+
+        $client->__setSoapHeaders($headers);
+        $params = array(
+            'queryType' => 1,
+            'query' => 'BSC=FIC*',
+            'startRecord' => '1',
+            'endRecord' => '9',
+            'sortField' => '',
+            'liveUpdate' => '',
+            'dataRequest' => 'IMG,IM60,IM90'
+        );
+
+        $res = $client->SearchRequestEnhanced($params);
+
+        $ingramSearch = new SimpleXMLElement($res->SearchRequestTypes12349EnhancedResult->any);
+Mage::register('ingram_category', $ingramSearch);
+
+//------------------------->>
+
         Mage::dispatchEvent('catalog_controller_category_init_before', array('controller_action' => $this));
         $categoryId = (int) $this->getRequest()->getParam('id', false);
         if (!$categoryId) {
